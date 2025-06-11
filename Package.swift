1d968cf3fb803780f74a8a2903c405ddc6cdd01d
Caglayan Sever <7887007+caglayansever@users.noreply.github.com>
2025-06-12 01:43:44 +0300

    Update Package.swift
    Fix SDWebImage version

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/zhangao0086/DKCamera", .branch("master")),
-        .package(url: "https://github.com/zhangao0086/DKPhotoGallery", .branch("master")),
+        .package(url: "https://github.com/caglayansever/DKPhotoGallery", .branch("develop")),
         .package(url: "https://github.com/TimOliver/TOCropViewController", from: "2.6.0"),
     ],
     targets: [
